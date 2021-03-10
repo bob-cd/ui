c9c2199d1717de1c50d47fc2d98a6a93eba577e6
Em Grasmeder <em@gras.org>
2021-03-10 21:32:38 +0100

    Move colors to their own file

---
+++ src/Colors.elm
@@ -0,0 +1,48 @@
+module Colors exposing (..)
+
+import Element exposing (..)
+
+
+lightBlue : Color
+lightBlue =
+    rgb255 70 108 169
+
+
+blue : Color
+blue =
+    rgb255 19 64 139
+
+
+darkBlue : Color
+darkBlue =
+    rgb255 5 36 84
+
+
+yellow : Color
+yellow =
+    rgb255 206 206 11
+
+
+gold : Color
+gold =
+    rgb255 206 135 11
+
+
+black : Color
+black =
+    rgb255 13 13 13
+
+
+purple : Color
+purple =
+    rgb255 94 14 137
+
+
+darkPurple : Color
+darkPurple =
+    rgb255 56 3 85
+
+
+lightPurple : Color
+lightPurple =
+    rgb255 134 67 170
--- src/Playground.elm
+++ src/Playground.elm
@@ -5,6 +5,7 @@
 
 module Playground exposing (main)
 
+import Colors exposing (..)
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
@@ -58,48 +59,3 @@ footer =
 colorify : String -> Color -> Element msg
 colorify text color =
     Element.el [ Font.color color ] (Element.text text)
-
-
-lightBlue : Color
-lightBlue =
-    rgb255 70 108 169
-
-
-blue : Color
-blue =
-    rgb255 19 64 139
-
-
-darkBlue : Color
-darkBlue =
-    rgb255 5 36 84
-
-
-yellow : Color
-yellow =
-    rgb255 206 206 11
-
-
-gold : Color
-gold =
-    rgb255 206 135 11
-
-
-black : Color
-black =
-    rgb255 13 13 13
-
-
-purple : Color
-purple =
-    rgb255 94 14 137
-
-
-darkPurple : Color
-darkPurple =
-    rgb255 56 3 85
-
-
-lightPurple : Color
-lightPurple =
-    rgb255 134 67 170
